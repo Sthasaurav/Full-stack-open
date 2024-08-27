e1cf5c2874f0dd3b2a7d9cd489b6d4b16aecd7ad
Sthasaurav <92661343+Sthasaurav@users.noreply.github.com>
2024-08-27 15:21:20 +0545

    complete exercise 2.5: Separate module step 10

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,42 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ parts }) => {
-
-  return (
-    <p>
-      total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercise
-      {/* {console.log('what is happening', sum, part)} */}
-    </p>
-
-  )
-}
-
-const Part = ({ part }) =>
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map((part) => (<Part key={part.id} part={part} />))}
-
-    </>)
-}
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-
-
-
-  )
-}
-
+import Course from "./components/Courses"
 const App = () => {
   const courses = [
     
@@ -87,7 +49,6 @@ const App = () => {
 
   return (
     <>
-      <h1>Web development curriculum</h1>
       {courses.map((course) => (<Course key={course.id} course={course} />))}
     </>
 
---
+++ part2/src/components/Courses.jsx
@@ -0,0 +1,51 @@
+const Header = ({ course }) =>{
+    return(
+        <>
+        
+        <h1>{course}</h1>
+        
+        </>
+        )
+         
+}
+
+
+const Total = ({ parts }) => {
+
+  return (
+    <p>
+      total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercise
+      {/* {console.log('what is happening', sum, part)} */}
+    </p>
+
+  )
+}
+
+const Part = ({ part }) =>
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (<Part key={part.id} part={part} />))}
+
+    </>)
+}
+
+const Course = ({ course }) => {
+  return (
+    <>
+
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+
+
+
+  )
+}
+
+export default Course
