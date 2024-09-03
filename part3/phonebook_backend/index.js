05fe2b08f81ebdf0b1ce8e12f3685538170e2e03
Sthasaurav <92661343+Sthasaurav@users.noreply.github.com>
2024-09-03 12:22:29 +0545

    added phonebook back in main folder

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -7,6 +7,7 @@ const cors =require("cors");
 app.use(cors());
 app.use(express.json());  
 app.use(bodyParser.urlencoded({ extended: true }));  
+app.use(express.static('dist'))
 app.use(morgan('tiny'));   
 let persons = [
   { 
@@ -87,4 +88,3 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-s
