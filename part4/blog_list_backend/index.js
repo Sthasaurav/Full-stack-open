b9908f05fc33138ec142203f1182acface5d1876
Sthasaurav <92661343+Sthasaurav@users.noreply.github.com>
2024-09-06 12:58:45 +0545

    module for config/logger

--- part4/blog_list_backend/index.js
+++ part4/blog_list_backend/index.js
@@ -1,9 +1,10 @@
-require("dotenv").config();
 
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger= require('./utils/logger')
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -14,8 +15,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.mongoUrl)
 
 app.use(cors())
 app.use(express.json())
@@ -38,7 +38,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+// const PORT = 3003
+app.listen(config.PORT, () => {
+logger.info(`Server running on port ${config.PORT} `)
 })
