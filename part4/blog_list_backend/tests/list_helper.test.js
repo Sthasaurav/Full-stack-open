32f4d21e5fbfe2407aa75146b3384c8f960945d2
Sthasaurav <92661343+Sthasaurav@users.noreply.github.com>
2024-09-09 13:25:30 +0545

    4.4: Helper Functions and Unit Tests, step 2

--- part4/blog_list_backend/tests/list_helper.test.js
+++ part4/blog_list_backend/tests/list_helper.test.js
@@ -2,6 +2,30 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+// const listWithOneBlog = [
+//   {
+//     _id: '5a422aa71b54a676234d17f8',
+//     title: 'Go To Statement Considered Harmful',
+//     author: 'Edsger W. Dijkstra',
+//     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+//     likes: 5,
+//     __v: 0
+//   }
+// ]
+
+
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+    __v: 0
+  },
+  
+]
+
 test('dummy returns one', () => {
   const blogs = [
     {
@@ -56,22 +80,11 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+describe('total likes', () => {
 
 
-// describe('total likes', () => {
-//   const listWithOneBlog = [
-//     {
-//       _id: '5a422aa71b54a676234d17f8',
-//       title: 'Go To Statement Considered Harmful',
-//       author: 'Edsger W. Dijkstra',
-//       url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-//       likes: 5,
-//       __v: 0
-//     }
-//   ]
-
-//   test('when list has only one blog, equals the likes of that', () => {
-//     const result = listHelper.totalLikes(listWithOneBlog)
-//     assert.strictEqual(result, 5)
-//   })
-// })
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+})
--- part4/blog_list_backend/utils/list_helper.js
+++ part4/blog_list_backend/utils/list_helper.js
@@ -3,11 +3,18 @@ const dummy = (blogs) => {
     return 1
 }
 
-  
-  const totalLikes=(blog)=>{
-    return (0)
+total=0
+const totalLikes = (blogs) =>  blogs.reduce((a, i) => a + i.likes, 0)
+  /*
+      total =0
+      1. iterate through blogs, 
+      2. add no of likes in each blog to total
+      return total
+    */
+// const favoriteBlog=(blogs)=>blogs
+ 
 
-  }
+  
 
   module.exports = {
     dummy,
