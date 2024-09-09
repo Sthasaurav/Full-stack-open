128a285c62bf0247ff323da71b240ab78a0cddcf
Sthasaurav <92661343+Sthasaurav@users.noreply.github.com>
2024-09-09 19:33:54 +0545

    total likes

--- part4/blog_list_backend/utils/list_helper.js
+++ part4/blog_list_backend/utils/list_helper.js
@@ -11,12 +11,13 @@ const totalLikes = (blogs) =>  blogs.reduce((a, i) => a + i.likes, 0)
       2. add no of likes in each blog to total
       return total
     */
-// const favoriteBlog=(blogs)=>blogs
+const favoriteBlog=(blogs)=>blogs.reduce((a,i) => a.likes > i.likes ? a : i, {})
  
 
-  
+
 
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
