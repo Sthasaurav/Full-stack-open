a584f594d4fd324747af3e527ef27a541aad68cb
Sthasaurav <92661343+Sthasaurav@users.noreply.github.com>
2024-10-06 12:09:02 +0545

    backend front end working

--- part9/Patientor_Backend/index.ts
+++ part9/Patientor_Backend/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 const app = express();
 app.use(express.json());
+app.use(express.static('dist'))
 
 app.get('/api/ping', (_req, res) => {
-    res.send('Hello fullstack open');
+    res.send('Hello fullstack open ping');
   });
 
   const PORT = 3003;
